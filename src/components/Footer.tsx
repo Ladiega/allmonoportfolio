2dc3cd201afa8581be1e4db5fe2aa2a5c7913f2b
xfokiux xfokiux <xfokiuxxfokiux@192.168.1.14>
2025-05-07 00:46:47 -0500

    footer fixed

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,45 +7,50 @@ import logo from "../../public/img/Mono.svg";
 
 export default function Footer() {
   return (
-    <footer className="bg-amber-400 flex justify-between items-center p-10 ">
-      <div>
-        <Image
-          src={logo}
-          alt="instagram"
-          width={40}
-          className="scale-x-[-1] "
-        />
-      </div>
-
-      <div className="flex gap-3">
+    <footer className="bg-amber-400  p-10 ">
+      <div className="flex justify-between items-center">
         <div>
-          <Link
-            href="https://www.instagram.com/all.mono/?hl=es-la"
-            target="_blank"
-          >
-            <Image src={instagram} alt="instagram" width={40} />
-          </Link>
+          <Image
+            src={logo}
+            alt="instagram"
+            width={40}
+            className="scale-x-[-1] "
+          />
         </div>
 
-        <div>
-          <Link href="https://vimeo.com/user171798796" target="_blank">
-            <Image src={vimeo} alt="instagram" width={40} />
-          </Link>
+        <div className="flex gap-3">
+          <div>
+            <Link
+              href="https://www.instagram.com/all.mono/?hl=es-la"
+              target="_blank"
+            >
+              <Image src={instagram} alt="instagram" width={40} />
+            </Link>
+          </div>
+
+          <div>
+            <Link href="https://vimeo.com/user171798796" target="_blank">
+              <Image src={vimeo} alt="instagram" width={40} />
+            </Link>
+          </div>
+        </div>
+        <div className="hidden md:block">
+          <ul className="flex  flex-col  font-light">
+            <li>
+              <Link href="/">PORTAFOLIO</Link>
+            </li>
+            <li>
+              <Link href="/">SOBRE MI</Link>
+            </li>
+            <li>
+              <Link href="/">CONTACTO</Link>
+            </li>
+          </ul>
         </div>
       </div>
-      <div className="hidden md:block">
-        <ul className="flex  flex-col  font-light">
-          <li>
-            <Link href="/">PORTAFOLIO</Link>
-          </li>
-          <li>
-            <Link href="/">SOBRE MI</Link>
-          </li>
-          <li>
-            <Link href="/">CONTACTO</Link>
-          </li>
-        </ul>
-      </div>
+      <p className="text-black text-center text-[11px] mt-5 font-extralight">
+        AllMono Copyright © All rights reserved.
+      </p>
     </footer>
   );
 }
